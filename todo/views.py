d0f64b88d5c953709aef408adb7e0188db36d56a
TechBenefits <77223001+TechBenefits@users.noreply.github.com>
2021-04-10 16:10:57 +0900

    init

---
+++ todo/views.py
@@ -0,0 +1,11 @@
+from todo.models import TodoModel
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import TodoModel
+
+# Create your views here.
+
+class TodoList(ListView):
+    template_name = 'list.html'
+    model = TodoModel
+
